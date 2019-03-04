d0e2492924e5e1fbfa39670452935bfcacb8aa7b
gwayne9955 <gwayne9955@yahoo.com>
2019-03-04 14:54:22 -0800

    cleaned up products ui

--- client/src/views/ProductDetail.vue
+++ client/src/views/ProductDetail.vue
@@ -1,19 +1,36 @@
 <template>
-  <div class="products">
-    <a class="button is-light" v-if="isLoggedIn" v-on:click="showEditProductModal()">Edit this Product</a>
-    <a class="button is-light" v-if="isLoggedIn" v-on:click="showDeleteProductModal()">Delete this Product</a>
-    <EditProduct v-bind:product="item" v-bind:is-showing="showEditProduct" v-on:success="successEditProduct()" v-on:cancel="cancelEditProduct()"/>
-    <DeleteProduct v-bind:product="item" v-bind:is-showing="showDeleteProduct" v-on:success="successDeleteProduct()" v-on:cancel="cancelDeleteProduct()"/>
-    <div class="container products-container">
-      <div class="tile is-ancestor">
-          
-          <div class="tile is-child box product">
-            <h2>{{this.item.name}}</h2>
-            <img class="product" :src="this.item.imageUrls">
-          </div>
-
+  <div class="product-detail">
+    <div class="columns">
+      <div class="column is-two-fifths">
+        <h3 class="title is-3">Product Details:</h3>
       </div>
-
+      <div class="column is-three-fifths">
+        <a class="button is-light edit-product" v-if="isLoggedIn" v-on:click="showEditProductModal()">Edit this Product</a>
+        <a class="button is-light" v-if="isLoggedIn" v-on:click="showDeleteProductModal()">Delete this Product</a>  
+      </div>
+    </div>
+    <div class="product-detail">
+      <EditProduct v-bind:product="item" v-bind:is-showing="showEditProduct" v-on:success="successEditProduct()" v-on:cancel="cancelEditProduct()"/>
+      <DeleteProduct v-bind:product="item" v-bind:is-showing="showDeleteProduct" v-on:success="successDeleteProduct()" v-on:cancel="cancelDeleteProduct()"/>
+      <div class="columns">
+        <div class="column is-two-fifths">
+          <img class="product-img" :src="this.item.imageUrls">
+        </div>
+        <div class="column is-three-fifths">
+          <div class="product-field product-name">
+            <h5 class="title is-5">Product name:</h5>
+            <h2>{{this.item.name}}</h2>
+          </div>
+          <div class="product-field product-description">
+            <h5 class="title is-5">Product description:</h5>
+            <h2>{{this.item.description}}</h2>
+          </div>
+          <div class="product-field product-price">
+            <h5 class="title is-5">Price:</h5>
+            <h2>${{this.item.price}}</h2>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -103,8 +120,12 @@ img.product {
   height: 200px;
 }
 
-.product:hover {
-  cursor: pointer
+.edit-product {
+  margin-right: 14px;
+}
+
+.product-field {
+  margin-bottom: 40px;
 }
 
 </style>
